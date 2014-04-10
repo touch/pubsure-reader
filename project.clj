d7a060980e3015276789f210b2c6d42cc66a2e75
Arnout Roemers <github@functionalbytes.nl>
2014-04-10 20:36:33 +0200

    Add pubsure-reader containium app, using pubsure-ws and pubsure-zk.

---
+++ project.clj
@@ -0,0 +1,16 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+(defproject pubsure-reader "0.1.0-SNAPSHOT"
+  :description "Containium app for pubsure-ws/reader using pubsure-zk."
+  :url "https://github.com/containium/Backend"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [containium "0.1.0-SNAPSHOT"]
+                 [pubsure/pubsure-ws "0.1.0-SNAPSHOT"]
+                 [pubsure/pubsure-zk "0.1.0-SNAPSHOT"]]
+  :containium {:start pubsure-reader.core/start
+              :stop pubsure-reader.core/stop
+              :ring {:handler pubsure-reader.core/app
+                     :context-path "/pubsure"}
+              :zk-connect-str "localhost:2181"})
---
+++ src/pubsure_reader/core.clj
@@ -0,0 +1,26 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+(ns pubsure-reader.core
+  (:require [pubsure-ws.reader :as reader]
+            [pubsure-zk.directory :as directory]
+            [containium.systems :refer (forward-protocol)]
+            [org.httpkit.server :as server]))
+
+
+(def app nil)
+
+(def stop nil)
+
+(defn start
+  "Start the app."
+  [systems config]
+  (let [zk-connect-str (-> config :containium :zk-connect-str)
+        zk-dir (directory/start-directory zk-connect-str)
+        [reader-state ring-app] (reader/make-app zk-dir)
+        stop-fn (fn [_]
+                  (reader/stop-app reader-state)
+                  (directory/stop-directory zk-dir))]
+    (alter-var-root #'app (constantly ring-app))
+    (alter-var-root #'stop (constantly stop-fn))))
