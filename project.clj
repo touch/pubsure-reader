aef3443e2f6a0a0d06dfada8538d1ed759cec259
Misha Vakulich <m.vakulich@softteco.com>
2019-10-15 02:47:08 +0300

    upgrade

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(defproject pubsure-reader "0.1.0-SNAPSHOT"
+(defproject pubsure-reader "0.2.0-SNAPSHOT"
   :description "Containium app for pubsure-ws/reader using pubsure-zk."
   :url "https://github.com/containium/Backend"
-  :dependencies [[containium "0.1.0-SNAPSHOT"]
+  :dependencies [[containium "0.2.0-SNAPSHOT"]
                  [pubsure/pubsure-ws "0.1.0-SNAPSHOT"]
                  [pubsure/pubsure-zk "0.1.0-SNAPSHOT"]]
   :exclusions [org.clojure/clojure]
